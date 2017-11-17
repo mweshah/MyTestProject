ab56299f50d512291f8173731455ae6b18c33269
Toshiba <Toshiba@Toshiba-PC.cs.mum.edu>
2017-11-16 21:08:09 -0600

    fi

---
+++ EmployeeValidDemo/src/main/java/edu/mum/controller/EmployeeController.java
@@ -0,0 +1,88 @@
+package edu.mum.controller;
+
+import java.io.File;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import edu.mum.domain.Employee;
+
+@Controller
+public class EmployeeController {
+
+	@Autowired
+	ServletContext servletContext;
+
+	private static final Log logger = LogFactory.getLog(EmployeeController.class);
+
+	@RequestMapping(value = { "/", "employee_input" })
+	public String inputEmployee(@ModelAttribute("employee") Employee employee) {
+		return "EmployeeForm";
+	}
+
+	@RequestMapping(value = "/employee_save")
+	public String saveEmployee(@Valid @ModelAttribute("employee") Employee employee, BindingResult bindingResult,
+			Model model) {
+
+		if (bindingResult.hasErrors()) {
+			return "EmployeeForm";
+		}
+
+		String[] suppressedFields = bindingResult.getSuppressedFields();
+		if (suppressedFields.length > 0) {
+			throw new RuntimeException("Attempt to bind fields that haven't been allowed in initBinder(): "
+					+ StringUtils.addStringToArray(suppressedFields, ", "));
+		}
+
+		// save product here
+		model.addAttribute("employee", employee);
+
+		MultipartFile productImage = employee.getProductImage();
+		String rootDirectory = servletContext.getRealPath("/");
+
+		// isEmpty means file exists BUT NO Content
+		if (productImage != null && !productImage.isEmpty()) {
+			try {
+				productImage.transferTo(new File(rootDirectory + "\\images\\" + employee.getId() + ".png"));
+			} catch (Exception e) {
+				throw new RuntimeException("Product Image saving failed", e);
+			}
+		}
+
+		return "EmployeeDetails";
+	}
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		// binder.setDisallowedFields(new String[]{"firstName"});
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ModelAndView handleError(HttpServletRequest req, Exception exception) {
+		ModelAndView mav = new ModelAndView();
+		
+		mav.addObject("msg", exception.getMessage());
+		mav.addObject("exception", exception);
+		mav.addObject("url", req.getRequestURL());
+		mav.setViewName("error2");
+		
+		return mav;
+	}
+
+}
